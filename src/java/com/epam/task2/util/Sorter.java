f1559ef68732f84438f945a52b884d644696bb2b
SERGEY-PC\serge <sergeypodgayskiy@gmail.com>
2017-03-28 00:28:35 +0300

    parametrized bubbleSort method was added

--- src/java/com/epam/task2/util/Sorter.java
+++ src/java/com/epam/task2/util/Sorter.java
@@ -1,8 +1,10 @@
 package com.epam.task2.util;
 
+import java.util.Comparator;
+
 /**
  * @author sergey
- * created on 24.03.2017
+ *         created on 24.03.2017
  */
 public final class Sorter {
 
@@ -28,7 +30,6 @@ public final class Sorter {
         int j;
 
         for (int i = 0; i < array.length - 1; i++) {
-
             if (array[i] > array[i + 1]) {
                 temp = array[i + 1];
                 array[i + 1] = array[i];
@@ -49,6 +50,24 @@ public final class Sorter {
                     swap(array, i, j);
                 }
             }
+        }
+    }
+
+    public static <T> void bubbleSort(T[] array, Comparator<T> comparator) {
+        try {
+            if (comparator == null) {
+                throw new NullPointerException("Comparator is null.");
+            } else {
+                for (int i = 0; i < array.length - 1; i++) {
+                    for (int j = i + 1; j < array.length; j++) {
+                        if (comparator.compare(array[i], array[j]) > 0) {
+                            swap(array, i, j);
+                        }
+                    }
+                }
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
     }
 
@@ -97,8 +116,14 @@ public final class Sorter {
         return (array != null || array.length != 0) && left <= right;
     }
 
-    private static void swap(int array[], int i, int j) {
+    private static void swap(int[] array, int i, int j) {
         int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
